85d9d2aac991e5a75cddbacfc94ae4d2df5add8a
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-18 14:07:42 +0900

    Lesson10

---
+++ src/src/Lesson10/Lesson10_01.java
@@ -0,0 +1,15 @@
+package Lesson10;
+
+public class Lesson10_01 {
+    public static void main(String[] args){
+        int age = 18;
+
+        if (age >= 20){
+            System.out.println("adult");
+        }else if (age == 0){
+            System.out.println("baby");
+        }else{
+            System.out.println("child");
+        }
+    }
+}
