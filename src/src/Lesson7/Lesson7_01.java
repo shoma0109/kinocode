07be59646b43991e2160d04b05ed17f1d2377674
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-18 13:31:37 +0900

    Lesson7

---
+++ src/src/Lesson7/Lesson7_01.java
@@ -0,0 +1,33 @@
+package Lesson7;
+
+public class Lesson7_01 {
+    public static void main(String[] args){
+        byte var01 = 1;
+        short var02 =12345;
+        int var03 = 1234567890;
+        long var04 = 123456789000L;
+        double var05 = 1.23456789;
+        float var06 = 1.234F;
+        char var07 = 'c';
+        String var08 = "hello world!";
+
+        int var_a = 10;
+        int var_b = 1;
+        boolean var_bool;
+        boolean var_bool_a;
+        var_bool = (var_a > var_b);
+        var_bool_a = (var_a < var_b);
+
+        System.out.println(var01);
+        System.out.println(var02);
+        System.out.println(var03);
+        System.out.println(var04);
+        System.out.println(var05);
+        System.out.println(var06);
+        System.out.println(var07);
+        System.out.println(var08);
+        System.out.println(var_bool);
+        System.out.println(var_bool_a);
+
+    }
+}
