b1b9d50346cddbb402aae35e3d8d89ee906035e7
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-18 13:17:32 +0900

    Lesson6

--- src/src/Greeting.java
+++ src/src/Greeting.java
@@ -1,12 +1,9 @@
 public class Greeting {
     public static void  main(String[] args){
-
         System.out.println("Good evening");
 
         System.out.println("Good Morning");
 
         System.out.println("Good afternoon");
-
-
     }
 }
---
+++ src/src/Lesson6/Lesson6_01.java
@@ -0,0 +1,25 @@
+package Lesson6;
+
+public class Lesson6_01 {
+    public static void main(String[] args){
+        int VAR = 0;
+        int var = 1;
+        int var01 = 2;
+        int var_01 = 3;
+        int var$01 = 4;
+//        int var-01 = 1;
+//        int 01var = 1;
+//        int return = 1;
+//        int class = 1;
+
+        System.out.println(VAR);
+        System.out.println(var);
+        System.out.println(var01);
+        System.out.println(var_01);
+        System.out.println(var$01);
+//        System.out.println(var-01);
+//        System.out.println(01var);
+//        System.out.println(return);
+//        System.out.println(class);
+    }
+}
