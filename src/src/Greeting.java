50f8a0a950dd8aedcf7dca8f7b80b1a347432f9e
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-18 13:00:34 +0900

    Lesson4

---
+++ src/src/Greeting.java
@@ -0,0 +1,12 @@
+public class Greeting {
+    public static void  main(String[] args){
+
+        System.out.println("Good evening");
+
+        System.out.println("Good Morning");
+
+        System.out.println("Good afternoon");
+
+
+    }
+}
