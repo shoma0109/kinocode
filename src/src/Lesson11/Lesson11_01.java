0ea8298881d1cc47d9df334e89fdfe3340a64e41
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-18 14:21:37 +0900

    Lesson11

---
+++ src/src/Lesson11/Lesson11_01.java
@@ -0,0 +1,11 @@
+package Lesson11;
+
+public class Lesson11_01 {
+    public static void  main(String[] args){
+        for(int i = 0; i <= 4; i ++){
+            if(i == 3){
+                break;
+            }
+            System.out.println(i);}
+    }
+}
---
+++ src/src/Lesson11/Lesson11_02.java
@@ -0,0 +1,11 @@
+package Lesson11;
+
+public class Lesson11_02 {
+    public static void  main(String[] args){
+        for(int i = 0; i <= 4; i ++){
+            if(i == 3){
+                continue;
+            }
+            System.out.println(i);}
+    }
+}
