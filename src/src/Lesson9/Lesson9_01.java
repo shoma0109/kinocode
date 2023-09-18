f0eb91c8ae788499bed60bd95ab70c8680063230
shoma.tanaka@airitech.co.jp <shoma.tanaka@airitech.co.jp>
2023-09-18 14:01:30 +0900

    Lesson9

---
+++ src/src/Lesson9/Lesson9_01.java
@@ -0,0 +1,41 @@
+package Lesson9;
+
+public class Lesson9_01 {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 2;
+        int z = 10;
+        int a = 8;
+        int b = 3;
+        int c = 8;
+        int d = 12;
+        int e = 20;
+        int f = 8;
+        int g = 8;
+        f++;
+        g--;
+
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+        System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x >= y);
+        System.out.println(x <= y);
+        System.out.println(x <= z);
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(a >= 5 && a <= 10);
+        System.out.println(b >= 5 && b <= 10);
+        System.out.println(b == 3 || b == 3);
+        System.out.println(b == 1 || b == 1);
+        System.out.println(c += 10);
+        System.out.println(d += e);
+        System.out.println(f);
+        System.out.println(g);
+
+    }
+}
